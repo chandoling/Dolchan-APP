923754cadb3fb8169dc92494f2ff7b972679e293
chandoling <159571699+chandoling@users.noreply.github.com>
2024-09-20 19:08:26 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('repeatTask').addEventListener('click', () => {
+    window.location.href = "repeat-task.html"; 
+});
+
+document.getElementById('scheduledTransfer').addEventListener('click', () => {
+    window.location.href = "schedule-task.html"; 
+});
+
+document.getElementById('hackingRescue').addEventListener('click', () => {
+    window.location.href = "hacking-Rescue.html"; 
+});
